b2abfca0acf974b20ea2976d4bd7db02216fd16a
NikkyXO <alexisdame2017@gmail.com>
2022-07-14 13:36:39 +0100

    Sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * @brief
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i = 0, j = 0;
+    int temp = 0;
+
+    for (; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
