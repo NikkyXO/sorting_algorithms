a61a064e3bb18512668b1011270f042ffad7d07d
NikkyXO <alexisdame2017@gmail.com>
2022-07-16 06:11:04 +0100

    fixed header file errors

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
@@ -24,6 +24,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort_rec(int *array, int lower, int higher, size_t size);
 int lomuto_partition(int *array, int lower, int higher, size_t size);
+void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
