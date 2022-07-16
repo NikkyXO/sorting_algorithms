9014653b4ef816e5f4f0a0ccffd7eed259e547f9
NikkyXO <alexisdame2017@gmail.com>
2022-07-16 05:55:59 +0100

    modified header file

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define __SORT_H
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -25,10 +26,11 @@ void quick_sort(int *array, size_t size);
 void quick_sort_rec(int *array, int lower, int higher, size_t size);
 int lomuto_partition(int *array, int lower, int higher, size_t size);
 void shell_sort(int *array, size_t size);
-void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void counting_sort(int *array, size_t size);
 void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
 void merge_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void quick_s(int *array, int lo, int hi, size_t size);
-int partition(int *array, int lo, int hi, size_t size);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
 #endif
