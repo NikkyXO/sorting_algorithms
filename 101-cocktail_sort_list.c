93fb214b4327e101bbeb3f1e5f9f5e5627237530
NikkyXO <alexisdame2017@gmail.com>
2022-07-16 05:44:51 +0100

    fixed betty coding style

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,10 +9,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quick_s(array, 0, size - 1, size);
+	quick_s(array, 0, size - 1, size);
 }
 
 /**
@@ -25,31 +25,31 @@ void quick_sort(int *array, size_t size)
  */
 int partition(int *array, int lo, int hi, size_t size)
 {
-    int i = lo - 1, j = lo;
-    int pivot = array[hi], aux = 0;
+	int i = lo - 1, j = lo;
+	int pivot = array[hi], aux = 0;
 
-    for (; j < hi; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (array[i] != array[j])
-            {
-                aux = array[i];
-                array[i] = array[j];
-                array[j] = aux;
-                print_array(array, size);
-            }
-        }
-    }
-    if (array[i + 1] != array[hi])
-    {
-        aux = array[i + 1];
-        array[i + 1] = array[hi];
-        array[hi] = aux;
-        print_array(array, size);
-    }
-    return (i + 1);
+	for (; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (array[i] != array[j])
+			{
+				aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[hi])
+	{
+		aux = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = aux;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -62,12 +62,12 @@ int partition(int *array, int lo, int hi, size_t size)
  */
 void quick_s(int *array, int lo, int hi, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (lo < hi)
-    {
-        pivot = partition(array, lo, hi, size);
-        quick_s(array, lo, pivot - 1, size);
-        quick_s(array, pivot + 1, hi, size);
-    }
+	if (lo < hi)
+	{
+		pivot = partition(array, lo, hi, size);
+		quick_s(array, lo, pivot - 1, size);
+		quick_s(array, pivot + 1, hi, size);
+	}
 }
